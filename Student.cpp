0257d4fa858d194046532d0e5d5b957f2320c497
Yaiyneabeba <139072009+Cep0110@users.noreply.github.com>
2023-09-13 15:17:18 +0300

    Change code Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,55 +1,40 @@
-#include <iostream>
-#include <string>
-
-class Student {
-private:
-    std::string name;
-    int age;
-    double grade;
-
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+    private:
+string name;
+int age;
+double grade;
 public:
-    // Setter function to set student's information
-    void setStudentInfo(const std::string& studentName, int studentAge, double studentGrade) {
-        name = studentName;
-        age = studentAge;
-        grade = studentGrade;
+Student(string stName,int stAge,double stGrade){
+    name=stName;
+    age=stAge;
+    grade=stGrade;
+}
+void displayInfo(){
+    cout<<"Name:"<<name<<endl;
+   cout<<"Age:"<<age<<endl;     cout<<"Grade:"<<grade<<endl;
+ }    
+double getGrade(){
+return grade;}
+double calculateAverage(Student & st1,Student & st2){
+    double total=st1.getGrade()+st2.getGrade();
+    int stCount=2;
+    if(stCount==0){
+        return 0;
     }
-
-    // Getter functions to retrieve student's information
-    std::string getName() const {
-        return name;
-    }
-
-    int getAge() const {
-        return age;
-    }
-
-    double getGrade() const {
-        return grade;
-    }
+    return total/stCount;
+}
 };
-
-int main() {
-    // Create student objects and set their information
-    Student student1;
-    student1.setStudentInfo(" Kaleb Tasew", 18, 85.5);
-
-    Student student2;
-    student2.setStudentInfo("Jemil Abrar", 17, 92.0);
-
-    Student student3;
-    student3.setStudentInfo("Biniyam Tesfaye", 19, 77.8);
-
-    // Display each student's information
-    std::cout << "Student 1: " << student1.getName() << ", Age: " << student1.getAge() << ", Grade: " << student1.getGrade() << std::endl;
-    std::cout << "Student 2: " << student2.getName() << ", Age: " << student2.getAge() << ", Grade: " << student2.getGrade() << std::endl;
-    std::cout << "Student 3: " << student3.getName() << ", Age: " << student3.getAge() << ", Grade: " << student3.getGrade() << std::endl;
-
-    // Calculate the average grade for the group of students
-    double totalGrade = student1.getGrade() + student2.getGrade() + student3.getGrade();
-    int numStudents = 3;
-    double averageGrade = (numStudents > 0) ? (totalGrade / numStudents) : 0.0;
-
-    std::cout << "Average Grade: " << averageGrade << std::endl;
-
-    return 0;
+int main(){
+    Student st1("Kalhab Fesiha",24,78.9);
+Student st2("Kalid Feyisa",29,90.9);
+st1.displayInfo();
+cout<<endl;
+st2.displayInfo();
+cout<<endl;
+double Average=st1.calculateAverage(st1,st2);
+cout<<"Average Grade:"<<Average<<endl;
+return 0;
+}
